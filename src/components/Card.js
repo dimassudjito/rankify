be3afdded852999215dfba3b072fc0fa9ef04925
dimas sudjito <dimassudjito@localhost.localdomain>
2021-02-20 14:14:17 -0700

    redesign color scheme

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,14 @@ const Card = ({item}) => {
 
     return (
         <div className="d-flex justify-content-center">
-            <div className="card m-5" style={{width: "18rem", color:"black"}}>
+            <div 
+                className="card m-5" 
+                style={{
+                    width: "18rem", 
+                    backgroundColor:"transparent",
+                    border: "5px solid white",
+                }}
+            >
                 <div className="card-body">
                     <h1 className="card-title text-center">{item}</h1>
                 </div>
